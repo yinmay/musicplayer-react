b66e422ffaafaf35c4713934b8ea15bdece27ecd
Mei <you@example.com>
2017-12-19 18:49:59 +0800

    progressbar

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 # 更新日志
 
 [2017-12-19]
-* 配置webpack
-* 
+* 配置webpack`hot-loader`,`webpack-dev-server`
+* header component
 
 
--- app/root.js
+++ app/root.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import Header from './compoents/header'
+import Progress from './compoents/progress'
 
 let Root = React.createClass({
+    getInitialState(){
+        return {
+            progress:'-'
+        }
+    },
+    componentDidMount(){
+        $('#player').jPlayer({
+            ready:function(){
+                $(this).jPlayer('setMedia', {
+                    // mp3:'http://tingwa.oss-cn-shanghai.aliyuncs.com/2017-01/05/20170105123005-MzkzMTcz.mp3?OSSAccessKeyId=3b1nzo7roav1h50rcp0a35nw&Expires=1511506386&Signature=WmcqXQ2v4wIMjuK1pOlvCwScLZ4%3D'
+                    mp3:'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
+                }).jPlayer('play')
+            },
+            supplied:'mp3',
+            vmode:'window'
+        })
+        $('#player').bind($.jPlayer.event.timeupdate, (e)=>{
+            this.setState({
+                progress:Math.round(e.jPlayer.status.currentPercentAbsolute)
+            })
+        })
+    },
     render(){
         return(
-            <Header />
+            <div>
+                <Header />
+                <div id="player"></div>
+                <Progress progress = {this.state.progress}>
+
+                </Progress>
+            </div>
+
         )
     }
 })
